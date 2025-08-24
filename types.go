de21c410ea2340251d2bcba3a69957042e51ee12
DoniLite <yaomessan13@outlook.com>
2025-08-24 10:00:29 +0000

    feat(event bus): add event subscription and handler management methods

--- types.go
+++ types.go
@@ -2,17 +2,22 @@ package goevents
 
 import "sync"
 
+// A single event in the event bus
 type Event struct {
 	Name string
 }
 
+// The data associated with an event
+// This data will be passed to the event handlers when the event is emitted
 type EventData struct {
 	Message string
 	Payload any
 }
 
+// A function that will be called when an event is emitted
 type EventHandler func(event *EventData, args ...string)
 
+// EventFactory is responsible for creating and managing events
 type EventFactory struct {
 	mu             *sync.Mutex
 	wg             *sync.WaitGroup
@@ -20,7 +25,7 @@ type EventFactory struct {
 	registeredFunc map[*Event][]EventHandler
 }
 
-
+// Decodes the payload of an EventData into the specified type
 func DecodeDataPayload[T any](data *EventData) T {
-	return  data.Payload.(T)
+	return data.Payload.(T)
 }
